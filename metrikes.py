09424210843e4fa697ac21c3f6c7c9f8a6206edc
nickpal <nickpsal@gmail.com>
2022-04-05 15:47:02 +0300

    changes 05/04/2022

--- metrikes.py
+++ metrikes.py
@@ -5,13 +5,13 @@ print("----------------------------------- Halstead Metrics --------------------
 print("We provide the numbers n1, n2, N1, N2 and the program calculates all the Halstead Metrics")
 while True:
     try:
-        n1, n2, N1, N2 = input("Give the n1, N2, N1, N2 values ").split(',')
+        n1, n2, N1, N2 = input("Give the n1, n2, N1, N2 values 0, 0, 0, 0 to exit ").split(',')
         n1 = float(n1)
         n2 = float(n2)
         N1 = float(N1)
         N2 = float(N2)
         if n1 < 0 or n2 < 0 or N1 < 0 or N2 < 0:
-            print("You vive a negative Value or zero Please give a positive Value")
+            print("You give a negative Value Please give a positive Value")
             continue
         elif n1 == 0 and n2 == 0 and N1 == 0 and N2 == 0:
             break
